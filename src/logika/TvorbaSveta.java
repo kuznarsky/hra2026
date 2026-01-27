d628e310b299e265457f6ae2eed2780570f40848
kuznarsky <kuznarsky@spsejecna.cz>
2026-01-27 08:14:58 +0100

    proc se to nepushuje

--- src/logika/TvorbaSveta.java
+++ src/logika/TvorbaSveta.java
@@ -2,5 +2,22 @@ package logika;
 
 import com.google.gson.Gson;
 
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.List;
+import java.util.Map;
+
 public class TvorbaSveta {
+    private class MistnostData {
+        String nazev;
+        String popis;
+        Map<String, String> vychody;
+    }
+    public Mistnost vytvorSvetZeSouboru(String cestaKSouboru) {
+        Gson gson = new Gson();
+        List<MistnostData> dataSeznam;
+        try (Reader reader = new FileReader(cestaKSouboru)) {
+            dataSeznam = gson.fromJson(reader)
+        }
+    }
 }
