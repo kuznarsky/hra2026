0d42a9debe67439548de8d64cc76449446cc9a62
kuznarsky <kuznarsky@spsejecna.cz>
2026-02-01 13:47:04 +0100

    joo logika pohybu funguje

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,5 +16,28 @@ public class Main {
             return;
         }
 
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n---------------------");
+            System.out.println("Jsi v: " + aktualniMistnost.getNazev());
+            System.out.println(aktualniMistnost.dlouhyPopis());
+            System.out.println("Vychody: " + aktualniMistnost.seznamVychodu());
+            System.out.println("Zadej smer (nebo 'konec'): ");
+
+            String vstup = sc.nextLine().trim();
+
+            if (vstup.equals("konec")) break;
+
+            Mistnost novaMistnost = aktualniMistnost.vratVychod(vstup);
+
+            if (novaMistnost == null) {
+                System.out.println("-> Timto smerem nejde jit");
+            } else {
+                aktualniMistnost = novaMistnost;
+            }
+        }
+
+        System.out.println("Hra ukoncena.");
     }
 }
